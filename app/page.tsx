963b289e626d8b04583a296361999c515b53ac51
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2023-05-10 13:07:16 -0300

    searchBar prototype and layout defined

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,40 @@
+'use client';
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Children, useState } from 'react'
+
 
 export default function Home() {
+
+  const [currentCity, setCurrentCity] = useState<string>();
+
+  const handleChange = (e:any) => {
+    setCurrentCity(e.target.value);
+    console.log(currentCity);
+  };
+
+  const formSubmit = () => {
+    console.log(currentCity);
+  };
+  
   return (
     <main>
-      <form>
+      <form className={styles.form}>
         <input 
           type='search'
           placeholder='Digite a cidade'
           required
           className={styles.search_bar}
+          onChange={handleChange}
         />
-        <input type='submit'/>
+        <button className={styles.submit} onClick={formSubmit}>
+          PROCURAR
+        </button>
       </form>
+      <section className={styles.weather_section}>
+        
+      </section>
     </main>
   )
 }
