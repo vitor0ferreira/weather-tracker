a68809e73c6f08a0fc390cc3e999c73122ba830d
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2023-05-12 00:30:58 -0300

    GeoCoding API pronta

--- app/page.tsx
+++ app/page.tsx
@@ -14,29 +14,38 @@ export default function Home() {
 
   const [searchCity, setSearchCity] = useState<string>('New York');
   const [cityCoordinates, setCityCoordinates] = useState<Cidade>();
-  const cardWeather:HTMLElement | null = document.getElementById('weather_card');
-  const geoApiURL:string = 'http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=5&appid={9db20493ccc761d551a7b7e55deaa7c2}';
+  const [isSearching, setIsSearching] = useState<boolean>(true);
+  const geoApiURL:string = `http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=5&appid=9db20493ccc761d551a7b7e55deaa7c2`;
+  const weatherApiURL:string = `https://api.openweathermap.org/data/3.0/onecall?lat=${cityCoordinates?.latitude}&lon=${cityCoordinates?.longitude}&appid=9db20493ccc761d551a7b7e55deaa7c2`
 
   const citySubmit = () => {
     console.log(searchCity);
   };
 
-  useEffect(()=>{
-    const getCoordinates = async () => {
+  const getCoordinates = async () => {
       
-      const geoLocationCall = await fetch(geoApiURL);
-      const locationData = await geoLocationCall.json();
-
-
-
-
-
-
+    const geoLocationCall = await fetch(geoApiURL);
+    const locationData = await geoLocationCall.json();
+    
+    const cidadeEncontrada:Cidade = {
+      latitude: locationData[0].lat,
+      longitude: locationData[0].lon
     };
 
-  },[]);
+    setCityCoordinates(cidadeEncontrada);
+  };
 
-  
+
+  useEffect(()=>{
+
+    if(isSearching){
+      getCoordinates();
+    }
+    
+  },[isSearching]);
+
+  console.log(cityCoordinates);
+
   return (
     <main>
       <div className={styles.div}>
